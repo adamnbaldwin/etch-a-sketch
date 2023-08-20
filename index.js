838c96147658e52ef73842f58341dc71b7f33aed
Adam Baldwin <adamnbaldwin@gmail.com>
2023-08-20 10:13:35 -0700

    add reset button function, add prompt function, update style flex properties

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
 let startButton = document.querySelector("#startButton");
 let resetButton = document.querySelector("#resetButton");
-let createButton = document.querySelector("#createButton");
 let gameContainer = document.querySelector("#container");
+let promptNumber;
 
 
 startButton.addEventListener("click", () => {
+clearGrid();
 createGrid()});
 
+function clearGrid(){
+    gameContainer.replaceChildren()
+};
+
 function createGrid() {
-    for (i=0; i<16; i++){
+    promptNumber = prompt("How many rows X columns do you want your etch-a-sketch to be?", "Enter a number here");
+    for (i=0; i<promptNumber; i++){
     let column = document.createElement("div");
     column.className = "moo";
     column.id = "columns";
     gameContainer.append(column);
-    for (j=0; j<16; j++) {
+    column.addEventListener("mouseover",() => column.style.background = "Blue" );
+    resetButton.addEventListener("click", () => column.style.background = "Red");
+    for (j=0; j<promptNumber; j++) {
     let row = document.createElement("div");
     row.className="moo";
     row.id= "rows";
     column.append(row);
-    }
-};
+    row.addEventListener("mouseover",(e) => row.style.background = "Blue")
+    resetButton.addEventListener("click", () => row.style.background = "Red");
+    };;
+    };
 }
-
-// function createRows () {
-//     for (i=0; i<16; i++) {
-//         // let row = document.createElement("div");
-//         // row.className = "moo";
-//         // row.id = "moo3";
-//         column.appendChild(row)}
-// }
-
-// let square = document.createElement("div");
-// square.className = "moo";
-// square.id = "moo2"
-// gameContainer.append(square);
-
-// square.addEventListener("mouseover",(e) => {square.style.background = "Blue"});
-
-// resetButton.addEventListener("click", () => {
-//     resetGrid()});
-// function resetGrid () {
-// square.style.background = "red"};
-// }
-// }
